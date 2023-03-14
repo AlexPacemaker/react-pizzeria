8ca87663f26dc25fa8f44444a74293f9b34c0f86
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-03-14 13:15:39 +0300

    search refactor

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,8 +1,11 @@
+//компонент поиска
+
 import React, { useCallback, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setSearchValue } from "../../redux/slices/searchSlice";
 import { debounce } from "lodash";
 import styles from "./Search.module.scss";
+import { SvgMaker } from "../SvgMaker";
 
 const Search: React.FC = () => {
   const dispatch = useDispatch();
@@ -10,6 +13,7 @@ const Search: React.FC = () => {
 
   const [value, setValue] = useState("");
 
+  //функция очистки поля поиска по нажатию
   const onClickClear = () => {
     dispatch(setSearchValue(""));
     setValue("");
@@ -24,6 +28,7 @@ const Search: React.FC = () => {
     []
   );
 
+  //функция контроллируемого ввода
   const onchangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
     updateSearchValue(event.target.value);
@@ -31,40 +36,9 @@ const Search: React.FC = () => {
 
   return (
     <div className={styles.root}>
-      <svg
-        className={styles.icon}
-        enableBackground='new 0 0 32 32'
-        id='EditableLine'
-        version='1.1'
-        viewBox='0 0 32 32'
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        <circle
-          cx='14'
-          cy='14'
-          fill='none'
-          id='XMLID_42_'
-          r='9'
-          stroke='#000000'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          strokeMiterlimit='10'
-          strokeWidth='2'
-        />
-        <line
-          fill='none'
-          id='XMLID_44_'
-          stroke='#000000'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          strokeMiterlimit='10'
-          strokeWidth='2'
-          x1='27'
-          x2='20.366'
-          y1='27'
-          y2='20.366'
-        />
-      </svg>
+      <div className={styles.magnifier}>
+        <SvgMaker id='magnifier' />
+      </div>
       <input
         ref={inputRef}
         value={value}
@@ -75,7 +49,7 @@ const Search: React.FC = () => {
       {value && (
         <svg
           onClick={onClickClear}
-          className={styles.clearIcon}
+          className={styles.clear}
           viewBox='0 0 20 20'
           xmlns='http://www.w3.org/2000/svg'
         >
